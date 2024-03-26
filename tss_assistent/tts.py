ec930ed4f6486a527f3f2d1784c4e5c1b9b1e4d7
wooseungw <woow070840@gmail.com>
2024-03-26 10:59:44 +0900

    12

---
+++ tss_assistent/tts.py
@@ -0,0 +1,18 @@
+from gtts import gTTS
+from openai import OpenAI
+from Key import Api_key
+
+key = Api_key()
+client = OpenAI(api_key= key.key)
+
+data = './tss_assistent/output.mp3'
+
+tts = gTTS(text="안녕하세요",lang='ko')
+
+tts.save(f"{data}")
+audio_file = open(f"{data}","rb")
+transcript = client.audio.transcriptions.create(model="whisper-1",file = audio_file)
+
+print(transcript.text)
+
+
